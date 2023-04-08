e30c2edc720e6312c3df1faa49fae9bf53020997
AMANKANOJIYA <aman.kanojiya4203@gmail.com>
2023-04-08 15:46:22 +0530

    NFT  minting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,27 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '../styles/Home.module.css';
-import MyHeader from '@/components/myheader';
-import CWbutton from '@/components/cw-button';
-import MNbutton from '@/components/mn-button';
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "../styles/Home.module.css";
+import styles_btn from "../styles/button.module.css";
+import MyHeader from "@/components/myheader";
+import CWbutton from "@/components/cw-button";
+import React, { useState } from "react";
+import { Web3Button } from "@web3modal/react";
+import { mintNFT } from "../../utils/interact.js";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div className="h-24 min-h-full">
-      <MyHeader/>
-      <div className={styles.backgroundImage}> 
-      </div>
+      <MyHeader />
+      <div className={styles.backgroundImage}></div>
       <div className={styles.box}>
-          <img src="/giphy.gif" alt="" className={styles.giphy}/>
-          {/* <button className={styles.buttono}>Connect Wallet</button> */}
-          <CWbutton/>
-          <MNbutton/>
-      </div> 
+        <img src="/giphy.gif" alt="" className={styles.giphy} />
+        <Web3Button />
+        <div className={styles_btn.button} onClick={mintNFT}>
+          <a>Mint NFT</a>
+        </div>
+      </div>
     </div>
-
-  )
+  );
 }
---
+++ utils/interact.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+import { pinJSONToIPFS } from "./pinata.js";
+
+const contractABI = require("../contract-abi.json");
+const contractAddress = "0xDDa67C91a31c1C2Aeb0536add0c77fb16Dd5DEb6";
+
+export const mintNFT = async () => {
+  //error handling
+  let url = "./publicgiphy.gif";
+  let name = "Cool NFT";
+  let description = "Monkey";
+  if (url.trim() == "" || name.trim() == "" || description.trim() == "") {
+    return {
+      success: false,
+      status: "❗Please make sure all fields are completed before minting.",
+    };
+  }
+
+  //make metadata
+  const metadata = new Object();
+  metadata.name = name;
+  metadata.image = url;
+  metadata.description = description;
+
+  //pinata pin request
+  const pinataResponse = await pinJSONToIPFS(metadata);
+  if (!pinataResponse.success) {
+    return {
+      success: false,
+      status: "😢 Something went wrong while uploading your tokenURI.",
+    };
+  }
+  const tokenURI = pinataResponse.pinataUrl;
+
+  //load smart contract
+  window.contract = await new web3.eth.Contract(contractABI, contractAddress); //loadContract();
+
+  //set up your Ethereum transaction
+  const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: window.ethereum.selectedAddress, // must match user's active address.
+    data: window.contract.methods
+      .mintNFT(window.ethereum.selectedAddress, tokenURI)
+      .encodeABI(), //make call to NFT smart contract
+  };
+
+  //sign transaction via MetaMask
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      success: true,
+      status:
+        "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" +
+        txHash,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message,
+    };
+  }
+};
